eb8d8dae65986a9250cae3d9ad248b275932f686
Vitor S M <vitorsilva3579@gmail.com>
2025-02-24 22:34:36 -0300

    User entity and resource

---
+++ src/main/java/com/springjpaeducandoweb/springjpacourse/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.springjpaeducandoweb.springjpacourse.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springjpaeducandoweb.springjpacourse.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Nome Teste", "Email@Teste.com", "9999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
