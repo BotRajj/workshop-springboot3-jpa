0851ecbd2f6104ea905c3ca7b3c110af11fe5bce
Vitor S M <vitorsilva3579@gmail.com>
2025-02-24 22:17:37 -0300

    Project created

---
+++ src/main/java/com/springjpaeducandoweb/springjpacourse/SpringjpacourseApplication.java
@@ -0,0 +1,13 @@
+package com.springjpaeducandoweb.springjpacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpacourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpacourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springjpaeducandoweb/springjpacourse/SpringjpacourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjpaeducandoweb.springjpacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpacourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
