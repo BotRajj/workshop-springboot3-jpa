441f33bf772945e35a77a2b88bf96068812ceaa4
Vitor S M <vitorsilva3579@gmail.com>
2025-02-27 23:00:55 -0300

    OrderStatus enum

--- src/main/java/com/springjpaeducandoweb/springjpacourse/config/TestConfig.java
+++ src/main/java/com/springjpaeducandoweb/springjpacourse/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.springjpaeducandoweb.springjpacourse.entities.Order;
 import com.springjpaeducandoweb.springjpacourse.entities.User;
+import com.springjpaeducandoweb.springjpacourse.entities.enums.OrderStatus;
 import com.springjpaeducandoweb.springjpacourse.repositories.OrderRepository;
 import com.springjpaeducandoweb.springjpacourse.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "UserTest1", "Test@test1.com", "11111", "1234");
 		User u2 = new User(null, "UserTest2", "Test@test2.com", "22222", "1234");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1); 
 	
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
