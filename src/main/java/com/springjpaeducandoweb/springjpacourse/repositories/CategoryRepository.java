0a94a7e04c4b3bdafc42b2f5f101a9c3f68221d1
Vitor S M <vitorsilva3579@gmail.com>
2025-02-27 23:36:33 -0300

    Category

---
+++ src/main/java/com/springjpaeducandoweb/springjpacourse/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.springjpaeducandoweb.springjpacourse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springjpaeducandoweb.springjpacourse.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
